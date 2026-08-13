eb686a7ab09e8299f97f6cac60caa0766ad3b605
Vivek <krishnacreations745@gmail.com>
2026-08-13 20:38:45 +0530

    delete product from cart when clicking delete

--- amazon-project-vivek/scripts/checkout.js
+++ amazon-project-vivek/scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart, removeFromCart } from "../data/cart.js";
 
 import { products } from "../data/products.js";
 
@@ -34,7 +34,7 @@ cart.forEach((cartItem) => {
                                     Quantity: <span class="quality-label">${cartItem.quantity}</span>
                                 </span>
                                 <span class="update-quantity-link link-primary">Update</span>
-                                <span class="delete-quantity-link link-primary">Delete</span>
+                                <span class="delete-quantity-link link-primary js-delete-link" data-product-id='${matchingProduct.id}'>Delete</span>
                             </div>
                         </div>
 
@@ -73,4 +73,12 @@ cart.forEach((cartItem) => {
     `;
 });
 
-document.querySelector(".js-order-summary").innerHTML = cartSummaryHTML;
+document.querySelector(".js-order-summary").innerHTML += cartSummaryHTML;
+
+document.querySelectorAll(".js-delete-link").forEach((link) => {
+  link.addEventListener("click", () => {
+    const productId = link.dataset.productId;
+
+    removeFromCart(productId);
+  });
+});
