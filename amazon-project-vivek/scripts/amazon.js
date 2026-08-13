1bb2f7d9488b971bd7104ab419f95f540db99d29
Vivek <krishnacreations745@gmail.com>
2026-08-13 20:25:17 +0530

    fixed price formatting and radio selectors

--- amazon-project-vivek/scripts/amazon.js
+++ amazon-project-vivek/scripts/amazon.js
@@ -3,6 +3,8 @@ import { cart, addToCart } from "../data/cart.js";
 
 import { products } from "../data/products.js";
 
+import { formatCurrency } from "./utils/money.js";
+
 let productsHTML = "";
 
 products.forEach((product) => {
@@ -22,7 +24,7 @@ products.forEach((product) => {
                     <div class="product-rating-count link-primary">${product.rating.count}</div>
                 </div>
 
-                <div class="product-price">$${(product.priceCents / 100).toFixed(2)}</div>
+                <div class="product-price">$${formatCurrency(matchingProduct.priceCents)}</div>
 
                 <div class="product-quantity-container">
                     <select class='js-product-quantity' data-product-id=${product.id}>
