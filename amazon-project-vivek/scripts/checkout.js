28b9db3380db322b807f6908acd5b1acf6e62454
Vivek <krishnacreations745@gmail.com>
2026-08-18 19:00:34 +0530

    learn promises and convert code to promises

--- amazon-project-vivek/scripts/checkout.js
+++ amazon-project-vivek/scripts/checkout.js
@@ -1,13 +1,60 @@
+import { loadCart } from "../data/cart.js";
 import { loadProducts } from "../data/products.js";
 import { renderOrderSummary } from "./Checkout/orderSummary.js";
-
 import { rederPaymentSummary } from "./Checkout/paymentSummary.js";
 
 // import "../data/cart-class.js";
 
 // import "../data/backend-practice.js";
 
-loadProducts(() => {
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve("value1");
+    });
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve("value2");
+    });
+  }),
+]).then((values) => {
+  console.log(values);
   renderOrderSummary();
   rederPaymentSummary();
 });
+
+/*
+
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve("value1");
+  });
+})
+
+  .then((value) => {
+    //whatever we give to resovle up is saved here
+    console.log(value); //display value1
+
+    return new Promise((resolve) => {
+      loadCart(() => {
+        resolve();
+      });
+    });
+  })
+
+  .then(() => {
+    renderOrderSummary();
+    rederPaymentSummary();
+  });
+*/
+/*
+loadProducts(() => {
+  loadCart(() => {
+    renderOrderSummary();
+    rederPaymentSummary();
+  });
+});
+*/
+
+//there is less nesting in promise which is better than using callback
