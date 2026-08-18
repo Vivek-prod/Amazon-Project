f8293692805cc348bb7e9e31cbaaaa066c25b902
Vivek <krishnacreations745@gmail.com>
2026-08-18 11:33:43 +0530

    learn more details about classes and this

--- amazon-project-vivek/data/products.js
+++ amazon-project-vivek/data/products.js
@@ -57,6 +57,42 @@ class Clothing extends Product {
   }
 }
 
+/*
+const date = new Date();
+console.log(date);
+console.log(date.toLocaleTimeString());
+console.log(date.toLocaleDateString());
+*/
+
+/*
+console.log(this);
+
+const object2 = {
+  a: 2,
+  b: this.a,
+};
+*/
+
+/*
+
+function logThis() {
+  console.log("hello");
+  console.log(this);
+}
+
+logThis();
+logThis.call("hello"); //this is changing the 'this'
+
+const object3 = {
+  method: () => {
+    console.log(this);
+  }, //arrow function  do not change the value of 'this'
+};
+
+object3.method();
+
+*/
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
