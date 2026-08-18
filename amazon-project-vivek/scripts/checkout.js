e76e9ba4ab11d4600b7527fdec0e3787c3ee212c
Vivek <krishnacreations745@gmail.com>
2026-08-18 10:06:56 +0530

    Create class version of cart

--- amazon-project-vivek/scripts/checkout.js
+++ amazon-project-vivek/scripts/checkout.js
@@ -2,7 +2,7 @@ import { renderOrderSummary } from "./Checkout/orderSummary.js";
 
 import { rederPaymentSummary } from "./Checkout/paymentSummary.js";
 
-import "../data/cart-oop.js";
+import "../data/cart-class.js";
 
 renderOrderSummary();
 
