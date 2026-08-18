a8cc4bda99d3462492252e369d9967bb84c1134e
Vivek <krishnacreations745@gmail.com>
2026-08-18 10:37:21 +0530

    convert products into classes

--- amazon-project-vivek/scripts/checkout.js
+++ amazon-project-vivek/scripts/checkout.js
@@ -2,7 +2,7 @@ import { renderOrderSummary } from "./Checkout/orderSummary.js";
 
 import { rederPaymentSummary } from "./Checkout/paymentSummary.js";
 
-import "../data/cart-class.js";
+// import "../data/cart-class.js";
 
 renderOrderSummary();
 
