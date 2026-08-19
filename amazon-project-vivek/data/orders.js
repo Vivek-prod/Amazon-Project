cae3a09e00b98e50c8df6dc4ff678eb86c21e750
Vivek <krishnacreations745@gmail.com>
2026-08-19 12:24:21 +0530

    create order using the backend

---
+++ amazon-project-vivek/data/orders.js
@@ -0,0 +1,10 @@
+export const orders = JSON.parse(localStorage.getItem("orders")) || [];
+
+export function addOrder(order) {
+  orders.unshift(order); //add the new product on the first
+  saveToStorage();
+}
+
+function saveToStorage() {
+  localStorage.setItem("orders", JSON.stringify(orders));
+}
--- amazon-project-vivek/scripts/Checkout/paymentSummary.js
+++ amazon-project-vivek/scripts/Checkout/paymentSummary.js
@@ -1,5 +1,6 @@
 import { calculateCartQuantity, cart } from "../../data/cart.js";
 import { getDeliveryOption } from "../../data/deliveryOptions.js";
+import { addOrder, orders } from "../../data/orders.js";
 
 import { getProduct } from "../../data/products.js";
 
@@ -53,9 +54,33 @@ export function rederPaymentSummary() {
         <div class="payment-summary-money">$${formatCurrency(totalCents)}</div>
     </div>
 
-    <button class="place-order-button button-primary">Place your order</button>
+    <button class="place-order-button button-primary js-place-order">Place your order</button>
   
   `;
 
   document.querySelector(".js-payment-summary").innerHTML = paymentSummaryHTML;
+
+  document
+    .querySelector(".js-place-order")
+    .addEventListener("click", async () => {
+      try {
+        const response = await fetch("https://supersimplebackend.dev/orders", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            cart: cart,
+          }),
+        });
+
+        const order = await response.json();
+        console.log(order);
+
+        addOrder(order);
+        window.location.href = "orders.html";
+      } catch (error) {
+        console.log("unexpected error", error);
+      }
+    });
 }
